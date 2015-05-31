b6257b1c37744ad1e925abf6853d24213ca6b814
Rob Patro <rob.patro@gmail.com>
2015-05-31 14:55:43 -0400

    re-split the library and program after fixing duplicate symbols

--- src/SalmonUtils.cpp
+++ src/SalmonUtils.cpp
@@ -69,6 +69,71 @@ namespace utils {
     }
 
 
+    /*
+    template< typename T >
+    TranscriptGeneMap transcriptToGeneMapFromFeatures(std::vector<GenomicFeature<T>> &feats ) {
+            using std::unordered_set;
+            using std::unordered_map;
+            using std::vector;
+            using std::tuple;
+            using std::string;
+            using std::get;
+
+            using NameID = tuple<string, size_t>;
+
+            IndexVector t2g;
+            NameVector transcriptNames;
+            NameVector geneNames;
+
+            // holds the mapping from transcript ID to gene ID
+            IndexVector t2gUnordered;
+            // holds the set of gene IDs
+            unordered_map<string, size_t> geneNameToID;
+
+            // To read the input and assign ids
+            size_t geneCounter = 0;
+            string transcript;
+            string gene;
+
+            std::sort( feats.begin(), feats.end(),
+                    []( const GenomicFeature<T> & a, const GenomicFeature<T> & b) -> bool {
+                    return a.sattr.transcript_id < b.sattr.transcript_id;
+                    } );
+
+            std::string currentTranscript = "";
+            for ( auto & feat : feats ) {
+
+                auto &gene = feat.sattr.gene_id;
+                auto &transcript = feat.sattr.transcript_id;
+
+                if ( transcript != currentTranscript ) {
+                    auto geneIt = geneNameToID.find(gene);
+                    size_t geneID = 0;
+
+                    if ( geneIt == geneNameToID.end() ) {
+                        // If we haven't seen this gene yet, give it a new ID
+                        geneNameToID[gene] = geneCounter;
+                        geneID = geneCounter;
+                        geneNames.push_back(gene);
+                        ++geneCounter;
+                    } else {
+                        // Otherwise lookup the ID
+                        geneID = geneIt->second;
+                    }
+
+                    transcriptNames.push_back(transcript);
+                    t2g.push_back(geneID);
+
+                    //++transcriptID;
+                    currentTranscript = transcript;
+                }
+
+            }
+
+            return TranscriptGeneMap(transcriptNames, geneNames, t2g);
+        }
+    */
+
     std::ostream& operator<<(std::ostream& os, OrphanStatus s) {
         switch (s) {
             case OrphanStatus::LeftOrphan:
