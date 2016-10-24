17052c215e4e4e71618fe93aed91597147e171a5
Rob Patro <rob-p@users.noreply.github.com>
2016-10-23 22:02:46 -0400

    Create dl_tut_reads.sh

---
+++ assets/dl_tut_reads.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir data
+cd data
+for i in `seq 25 40`; 
+do 
+  mkdir DRR0161${i}; 
+  cd DRR0161${i}; 
+  wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR016/DRR0161${i}/DRR0161${i}_1.fastq.gz; 
+  wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/DRR016/DRR0161${i}/DRR0161${i}_2.fastq.gz; 
+  cd ..; 
+done
+cd .. 
