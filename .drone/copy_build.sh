f02624798f94135c7cd33f7cb626ee35b189ac53
Rob Patro <rob@newton>
2016-11-07 17:59:25 -0500

    use DRONE_COMMIT_BRANCH rather than DRONE_REPO_BRANCH

--- .drone/copy_build.sh
+++ .drone/copy_build.sh
@@ -6,5 +6,5 @@ set -e
 cd scripts
 bash make-release.sh -v latest -n linux_x86_64
 cd ../RELEASES
-mkdir -p "/mnt/ci_res/${DRONE_REPO}/${DRONE_REPO_BRANCH}/build"
-cp *.tar.gz "/mnt/ci_res/${DRONE_REPO}/${DRONE_REPO_BRANCH}/build/"
+mkdir -p "/mnt/ci_res/${DRONE_REPO}/${DRONE_COMMIT_BRANCH}/build"
+cp *.tar.gz "/mnt/ci_res/${DRONE_REPO}/${DRONE_COMMIT_BRANCH}/build/"
