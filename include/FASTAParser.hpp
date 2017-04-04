c2858b7d91463270f5602a7157bb9938ca754a34
k3yavi <avi.srivastava@stonybrook.edu>
2017-04-04 18:01:20 -0400

    fix Avi's errors

--- include/FASTAParser.hpp
+++ include/FASTAParser.hpp
@@ -9,10 +9,11 @@ class SalmonOpts;
 class FASTAParser {
 public:
     FASTAParser(const std::string& fname);
-    void populateTargets(std::vector<Transcript>& transcripts, SalmonOpts& sopt);
+    void populateTargets(std::vector<Transcript>& transcripts, SalmonOpts& sopt,
+                        std::unordered_map<uint32_t,uint32_t> & alleleToSuperTxpMap);
 
 private:
     std::string fname_;
 };
 
-#endif // FASTA_PARSER 
+#endif // FASTA_PARSER
