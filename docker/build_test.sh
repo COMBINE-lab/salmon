ec9ac7054e4a2f4b1c2c09d53d3d5456de617611
Rob Patro <rob@newton>
2017-11-25 22:02:24 -0500

    back to development

--- docker/build_test.sh
+++ docker/build_test.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-docker build -t combinelab/salmon:0.8.2 .
+docker build -t combinelab/salmon:0.9.0 .
--- scripts/fetchRapMap.sh
+++ scripts/fetchRapMap.sh
@@ -23,8 +23,8 @@ fi
 
 mkdir -p ${EXTERNAL_DIR}
 #curl -k -L https://github.com/COMBINE-lab/RapMap/archive/salmon-v0.8.2.zip -o ${EXTERNAL_DIR}/rapmap.zip
-curl -k -L https://github.com/COMBINE-lab/RapMap/archive/salmon-v0.9.0.zip -o ${EXTERNAL_DIR}/rapmap.zip
-#curl -k -L https://github.com/COMBINE-lab/RapMap/archive/develop-salmon.zip -o ${EXTERNAL_DIR}/rapmap.zip
+#curl -k -L https://github.com/COMBINE-lab/RapMap/archive/salmon-v0.9.0.zip -o ${EXTERNAL_DIR}/rapmap.zip
+curl -k -L https://github.com/COMBINE-lab/RapMap/archive/develop-salmon.zip -o ${EXTERNAL_DIR}/rapmap.zip
 
 hashcheck=""
 if exists sha256sum; then
@@ -39,16 +39,16 @@ if [ -z "${hashcheck-}" ]; then
     echo "Couldn't find shasum command; can't verify contents of downloaded RapMap";
 else
     #echo "1691f4bca2b604f05f36772ae45faf0842ab4809843df770bd10366a5cfd6822  ${EXTERNAL_DIR}/rapmap.zip" | ${hashcheck} -c - || { echo "rapmap.zip did not match expected SHA1! Exiting."; exit 1; }
-    #echo "5e0fa601f97c7b5f3bfa3216755d20a957d3f5e761f1128566a1441145cb60d7  ${EXTERNAL_DIR}/rapmap.zip" | ${hashcheck} -c - || { echo "rapmap.zip did not match expected SHA1! Exiting."; exit 1; }
-    echo "e3dd29e3f79ad93f655c2262f428688d3dd81462d5ba3b7191dc1b16cbe5489f  ${EXTERNAL_DIR}/rapmap.zip" | ${hashcheck} -c - || { echo "rapmap.zip did not match expected SHA1! Exiting."; exit 1; }
+    echo "627e76da308c020fd475174afbec23448f526053882917b31b5dcfd20cfa33d5 ${EXTERNAL_DIR}/rapmap.zip" | ${hashcheck} -c - || { echo "rapmap.zip did not match expected SHA1! Exiting."; exit 1; }
+    #echo "e3dd29e3f79ad93f655c2262f428688d3dd81462d5ba3b7191dc1b16cbe5489f  ${EXTERNAL_DIR}/rapmap.zip" | ${hashcheck} -c - || { echo "rapmap.zip did not match expected SHA1! Exiting."; exit 1; }
     #echo "not testing sha in develop branch"
 fi
 
 
 rm -fr ${EXTERNAL_DIR}/RapMap
 unzip ${EXTERNAL_DIR}/rapmap.zip -d ${EXTERNAL_DIR}
-#mv ${EXTERNAL_DIR}/RapMap-develop-salmon ${EXTERNAL_DIR}/RapMap
-mv ${EXTERNAL_DIR}/RapMap-salmon-v0.9.0 ${EXTERNAL_DIR}/RapMap
+mv ${EXTERNAL_DIR}/RapMap-develop-salmon ${EXTERNAL_DIR}/RapMap
+#mv ${EXTERNAL_DIR}/RapMap-salmon-v0.9.0 ${EXTERNAL_DIR}/RapMap
 
 mkdir -p ${INSTALL_DIR}/include/rapmap
 mkdir -p ${INSTALL_DIR}/src/rapmap
