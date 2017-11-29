c178e00d9c4e3ad1593f4bcd465bc5c1e1ddfcf5
Rob Patro <rob@newton>
2017-11-28 22:10:19 -0500

    update fetchRapMap script

--- scripts/fetchRapMap.sh
+++ scripts/fetchRapMap.sh
@@ -21,10 +21,12 @@ if [ -d ${INSTALL_DIR}/src/rapmap ] ; then
     rm -fr ${INSTALL_DIR}/src/rapmap
 fi
 
+SVER=salmon-v0.9.1
+
 mkdir -p ${EXTERNAL_DIR}
 #curl -k -L https://github.com/COMBINE-lab/RapMap/archive/salmon-v0.8.2.zip -o ${EXTERNAL_DIR}/rapmap.zip
-#curl -k -L https://github.com/COMBINE-lab/RapMap/archive/salmon-v0.9.0.zip -o ${EXTERNAL_DIR}/rapmap.zip
-curl -k -L https://github.com/COMBINE-lab/RapMap/archive/develop-salmon.zip -o ${EXTERNAL_DIR}/rapmap.zip
+curl -k -L https://github.com/COMBINE-lab/RapMap/archive/${SVER}.zip -o ${EXTERNAL_DIR}/rapmap.zip
+#curl -k -L https://github.com/COMBINE-lab/RapMap/archive/develop-salmon.zip -o ${EXTERNAL_DIR}/rapmap.zip
 
 hashcheck=""
 if exists sha256sum; then
@@ -39,16 +41,16 @@ if [ -z "${hashcheck-}" ]; then
     echo "Couldn't find shasum command; can't verify contents of downloaded RapMap";
 else
     #echo "1691f4bca2b604f05f36772ae45faf0842ab4809843df770bd10366a5cfd6822  ${EXTERNAL_DIR}/rapmap.zip" | ${hashcheck} -c - || { echo "rapmap.zip did not match expected SHA1! Exiting."; exit 1; }
-    echo "627e76da308c020fd475174afbec23448f526053882917b31b5dcfd20cfa33d5 ${EXTERNAL_DIR}/rapmap.zip" | ${hashcheck} -c - || { echo "rapmap.zip did not match expected SHA1! Exiting."; exit 1; }
-    #echo "e3dd29e3f79ad93f655c2262f428688d3dd81462d5ba3b7191dc1b16cbe5489f  ${EXTERNAL_DIR}/rapmap.zip" | ${hashcheck} -c - || { echo "rapmap.zip did not match expected SHA1! Exiting."; exit 1; }
+    #echo "627e76da308c020fd475174afbec23448f526053882917b31b5dcfd20cfa33d5 ${EXTERNAL_DIR}/rapmap.zip" | ${hashcheck} -c - || { echo "rapmap.zip did not match expected SHA1! Exiting."; exit 1; }
+    echo "8975e5a1ed61ed9354ba776272927545f417ecdce95823e71ba1e7b61de7d380 ${EXTERNAL_DIR}/rapmap.zip" | ${hashcheck} -c - || { echo "rapmap.zip did not match expected SHA1! Exiting."; exit 1; }
     #echo "not testing sha in develop branch"
 fi
 
 
 rm -fr ${EXTERNAL_DIR}/RapMap
 unzip ${EXTERNAL_DIR}/rapmap.zip -d ${EXTERNAL_DIR}
-mv ${EXTERNAL_DIR}/RapMap-develop-salmon ${EXTERNAL_DIR}/RapMap
-#mv ${EXTERNAL_DIR}/RapMap-salmon-v0.9.0 ${EXTERNAL_DIR}/RapMap
+#mv ${EXTERNAL_DIR}/RapMap-develop-salmon ${EXTERNAL_DIR}/RapMap
+mv ${EXTERNAL_DIR}/RapMap-${SVER} ${EXTERNAL_DIR}/RapMap
 
 mkdir -p ${INSTALL_DIR}/include/rapmap
 mkdir -p ${INSTALL_DIR}/src/rapmap
@@ -63,3 +65,10 @@ cp -r ${EXTERNAL_DIR}/RapMap/include/tclap ${INSTALL_DIR}/include/rapmap
 cp -r ${EXTERNAL_DIR}/RapMap/include/*.h ${INSTALL_DIR}/include/rapmap
 cp -r ${EXTERNAL_DIR}/RapMap/include/*.hpp ${INSTALL_DIR}/include/rapmap
 cp -r ${EXTERNAL_DIR}/RapMap/include/sparsepp ${INSTALL_DIR}/include/rapmap
+##
+# Remove some redundant files that might otherwise be duplicated
+##
+rm ${INSTALL_DIR}/include/rapmap/FastxParser.hpp
+rm ${INSTALL_DIR}/include/rapmap/concurrentqueue.h
+rm ${INSTALL_DIR}/include/rapmap/FastxParserThreadUtils.hpp
+rm ${INSTALL_DIR}/src/rapmap/FastxParser.cpp
