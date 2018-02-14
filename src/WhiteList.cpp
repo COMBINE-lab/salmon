841016058068acb93b02e5d9e49bcf9929dc1f52
k3yavi <avi.srivastava@stonybrook.edu>
2018-02-14 11:29:59 -0500

    intial naive bayes; breaking all over the place

--- src/WhiteList.cpp
+++ src/WhiteList.cpp
@@ -1,4 +1,5 @@
 #include "WhiteList.hpp"
+#include <cmath>
 
 namespace alevin {
   namespace whitelist {
@@ -6,6 +7,89 @@ namespace alevin {
     void getCorrelation(std::vector<double>& A,
                         std::vector<double>& B){
       //calculate pearsonr
+    }
+
+    use DoubleMatrixT std::vector<std::vector<double>> ;
+    use DoubleVectorT std::vector<double> ;
+
+    double get_log_likelihood(double prior,
+                              DoubleVectorT& sigma,
+                              DoubleVectorT& theta,
+                              DoubleVectorT& query){
+      double logPrior = std::log(prior);
+      double mean, variance, likelihood {0.5};
+
+      // Gaussian Probability
+      for (size_t i=0; i<sigma.len(); i++){
+        variance = sigma[i];
+        mean = theta[i];
+
+        likelihood += std::log(2.0 * M_PI * variance);
+        likelihood += std::pow((query[i] - mean), 2) / variance;
+      }
+      likelihood *= -0.5;
+
+      return likelihood+logPrior;
+    }
+
+    bool naive_bayes_predict(DoubleMatrixT& sigma,
+                             DoubleMatrixT& theta,
+                             DoubleVectorT& query,
+                             DoubleVectorT& classPrior){
+      std::assert(sigma.size() == theta.size());
+      std::assert(query.size() == theta[0].size());
+
+      double trueProbability = get_log_likelihood(classPrior[0],
+                                                  sigma[0],
+                                                  theta[0],
+                                                  query);
+      double falseProbability = get_log_likelihood(classPrior[1],
+                                                   sigma[1],
+                                                   theta[1],
+                                                   query);
+
+      if ( trueProbability < falseProbability ){
+        return false;
+      }
+      else{
+        return true;
+      }
+    }
+
+    void update_mean_variance(DoubleMatrixT& features,
+                              DoubleMatrixT& theta,
+                              DoubleMatrixT& sigma,
+                              DoubleVectorT& classCount,
+                              uint8_t is_true){
+      std::assert( theta.size() == sigma.size() );
+      size_t numFeatures = features[0].size();
+
+      for ( auto& feature: features ){
+        for (size_t j=0; j<numFeatures; j++){
+          theta[is_true][i] += feature[i];
+        }
+      }
+    }
+
+    // Implementation from https://github.com/scikit-learn/scikit-learn/blob/master/sklearn/naive_bayes.py
+    void naive_bayes_fit(DoubleMatrixT& positiveFeatures,
+                         DoubleMatrixT& negativeFeatures){
+      size_t numFeatures { positiveFeatures[0].size() };
+      size_t numClasses {2};
+
+      DoubleMatrixT theta (numClasses, DoubleVectorT (numFeatures, 0.0));
+      DoubleMatrixT sigma (numClasses, DoubleVectorT (numFeatures, 0.0));
+
+      std::vector<uint32_t> classCount (numClasses, 0);
+      std::vector<double> classPrior (numClasses, 0.0);
+
+      update_mean_variance(positiveFeatures, theta, sigma, classCount, 1);
+      update_mean_variance(negativeFeatures, theta, sigma, classCount, 0);
+
+      double var_smoothing {1e-9};
+      // get mean across full dataset (i.e. both +ve and -ve)
+      double maxIntraFeatureVariance = std::max(mean);
+      double epsilon = var_smoothing * maxIntraFeatureVariance;
     }
 
     std::vector<uint32_t> classifyBarcodes(spp::sparse_hash_map<int32_t, std::vector<double>>& featureCountsMatrix,
