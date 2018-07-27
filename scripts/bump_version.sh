8cd465f85497bc9353c7c2e432eeeea09f57d328
Rob Patro <rob@newton>
2018-07-26 21:35:52 -0400

    [CI SKIP] improve bump_version.sh

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -1,8 +1,21 @@
+#!/usr/bin/env bash
 
-major_v=`../build/src/salmon -v | cut -d ' ' -f 2 | cut -d '.' -f 1`
-minor_v=`../build/src/salmon -v | cut -d ' ' -f 2 | cut -d '.' -f 2`
-patch_v=`../build/src/salmon -v | cut -d ' ' -f 2 | cut -d '.' -f 3`
+HERE="$( cd "$(dirname "$0")" ; pwd -P )"
+
+major_v=`${HERE}/../build/src/salmon -v | cut -d ' ' -f 2 | cut -d '.' -f 1`
+minor_v=`${HERE}/../build/src/salmon -v | cut -d ' ' -f 2 | cut -d '.' -f 2`
+patch_v=`${HERE}/../build/src/salmon -v | cut -d ' ' -f 2 | cut -d '.' -f 3`
 
 echo "VERSION : ${major_v}.${minor_v}.${patch_v}"
 
-awk -v majv=${major_v} -v minv=${minor_v} -v patchv=${patch_v} '{ if ($0 ~ /ENV SALMON_VERSION/) { print "ENV SALMON_VERSION " majv"."minv"."patchv; } else { print $0; }}' ../docker/Dockerfile > ../docker/Dockerfile.new
+# update docker file
+awk -v majv=${major_v} -v minv=${minor_v} -v patchv=${patch_v} '{ if ($0 ~ /ENV SALMON_VERSION/) { print "ENV SALMON_VERSION " majv"."minv"."patchv; } else { print $0; }}' \
+    ${HERE}/../docker/Dockerfile > ${HERE}/../docker/Dockerfile.new && mv ${HERE}/../docker/Dockerfile.new ${HERE}/../docker/Dockerfile
+
+# update version file (which feeds cmake)
+echo -e "VERSION_MAJOR ${major_v}\nVERSION_MINOR ${minor_v}\nVERSION_PATCH ${patch_v}" > ${HERE}/../current_version.txt
+
+# update conf.py
+awk -v majv=${major_v} -v minv=${minor_v} -v patchv=${patch_v} \
+    '{ if ($0 ~ /version = /) { print "version = '\''" majv"."minv"'\''"; } else if ($0 ~ /release = /) { print "release = '\''" majv"."minv"."patchv"'\''"; } else { print $0; }}' \
+    ${HERE}/../doc/source/conf.py > ${HERE}/../doc/source/conf.py.new && mv ${HERE}/../doc/source/conf.py.new ${HERE}/../doc/source/conf.py
