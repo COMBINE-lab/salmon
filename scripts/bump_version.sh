1c5457179be9df3f811fb8e4aad3d3619d7d2c22
Rob Patro <rob@newton>
2018-08-04 14:01:50 -0400

    [CI SKIP] improve bump_version script

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -12,6 +12,10 @@ echo "VERSION : ${major_v}.${minor_v}.${patch_v}"
 awk -v majv=${major_v} -v minv=${minor_v} -v patchv=${patch_v} '{ if ($0 ~ /ENV SALMON_VERSION/) { print "ENV SALMON_VERSION " majv"."minv"."patchv; } else { print $0; }}' \
     ${HERE}/../docker/Dockerfile > ${HERE}/../docker/Dockerfile.new && mv ${HERE}/../docker/Dockerfile.new ${HERE}/../docker/Dockerfile
 
+# update docker build script
+awk -v majv=${major_v} -v minv=${minor_v} -v patchv=${patch_v} '{ if ($0 ~ /SALMON_VERSION=/) { print "SALMON_VERSION="majv"."minv"."patchv; } else { print $0; }}' \
+    ${HERE}/../docker/build_test.sh > ${HERE}/../docker/build_test.sh.new && mv ${HERE}/../docker/build_test.sh.new ${HERE}/../docker/build_test.sh
+
 # update version file (which feeds cmake)
 echo -e "VERSION_MAJOR ${major_v}\nVERSION_MINOR ${minor_v}\nVERSION_PATCH ${patch_v}" > ${HERE}/../current_version.txt
 
