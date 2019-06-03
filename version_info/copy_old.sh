d15193d33f165b1d4a37bdc407efa64f6e3f8c48
Rob Patro <rob.patro@cs.stonybrook.edu>
2019-06-03 11:41:01 -0400

    update version message

---
+++ version_info/copy_old.sh
@@ -0,0 +1,15 @@
+cp old_message 0.10.0
+cp old_message 0.10.1
+cp old_message 0.10.2
+cp old_message 0.10.3
+cp old_message 0.11.0
+cp old_message 0.11.1
+cp old_message 0.11.2
+cp old_message 0.11.3
+cp old_message 0.12.0
+cp old_message 0.13.0
+cp old_message 0.13.1
+cp old_message 0.8.1
+cp old_message 0.8.2
+cp old_message 0.9.0
+cp old_message 0.9.1
