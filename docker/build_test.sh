f8b48ebbb088d4468a52ffbe57eb9b5df725e554
Rob Patro <rob@newton>
2019-06-21 12:08:29 -0400

    [CI SKIP] add merge_into_master script

--- docker/build_test.sh
+++ docker/build_test.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 SALMON_VERSION=0.14.0
-docker build -t combinelab/salmon:${SALMON_VERSION} -t combinelab/salmon:latest .
+docker build --no-cache -t combinelab/salmon:${SALMON_VERSION} -t combinelab/salmon:latest .
