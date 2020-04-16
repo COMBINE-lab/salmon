cb7caae45252dd5317b490a91d567fa1be6f79f7
Rob Patro <rob-p@users.noreply.github.com>
2020-04-15 20:37:57 -0400

    Update quant_tut_samples.sh

--- assets/quant_tut_samples.sh
+++ assets/quant_tut_samples.sh
@@ -3,8 +3,8 @@ for fn in data/DRR0161{25..40};
 do
 samp=`basename ${fn}`
 echo "Processing sample ${samp}"
-sailfish quant -i athal_index -l A \
+salmon quant -i athal_index -l A \
          -1 ${fn}/${samp}_1.fastq.gz \
          -2 ${samp}_2.fastq.gz \
-		 -p 8 -o quants/${samp}_quant
+		 -p 12 -o quants/${samp}_quant
 done 
