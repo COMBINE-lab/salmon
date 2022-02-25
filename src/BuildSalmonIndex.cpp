a76b480c1670ca5e70c3c3bb0f49779ade41cd3b
Rob Patro <rob@newton>
2022-02-24 22:47:10 -0500

    add no-clip argument to indexer options

--- src/BuildSalmonIndex.cpp
+++ src/BuildSalmonIndex.cpp
@@ -114,6 +114,8 @@ int salmonIndex(int argc, const char* argv[], std::unique_ptr<SalmonIndex>& /* s
       "Treat these sequences ids from the reference as the decoys that may "
       "have sequence homologous to some known transcript. for example in "
       "case of the genome, provide a list of chromosome name --- one per line")
+    ("no-clip,n", po::bool_switch(&idxOpt.noclip_polya)->default_value(false),
+      "Don't clip poly-A tails from the ends of target sequences")
     ("type",
       po::value<string>(&indexTypeStr)->default_value("puff")->required(),
       "The type of index to build; the only option is \"puff\" in this version "
