4169d812c93d3ef66417d65c61a76f6f5d4fced1
Rob Patro <rob@newton>
2026-03-10 13:37:26 -0400

    update hash

--- cmake/SalmonDependencies.cmake
+++ cmake/SalmonDependencies.cmake
@@ -12,7 +12,7 @@ set(SALMON_PUFFERFISH_GIT_REPOSITORY
     "https://github.com/COMBINE-lab/pufferfish.git"
     CACHE STRING "Git repository used when fetching pufferfish")
 set(SALMON_PUFFERFISH_GIT_TAG
-    "8070da7ff9124ce9322416623877424a04d1d162"
+    "c846e90cf943b403c80caafdb13f28f9a997060e"
     CACHE STRING "Immutable git commit used when fetching pufferfish")
 set(SALMON_PUFFERFISH_SOURCE_DIR
     ""
