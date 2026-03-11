aac21be4e42a3ea22a76a22a9b752a66fff73205
Rob Patro <rob@newton>
2026-03-11 02:50:21 -0400

    [release v1.11.4] fix libgff linking

--- docker/build_test.sh
+++ docker/build_test.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-SALMON_VERSION=1.11.0
+SALMON_VERSION=1.11.4
 TMPDIR=/mnt/scratch7/DELETE_ME_TEMP docker build --no-cache -t combinelab/salmon:${SALMON_VERSION} -t combinelab/salmon:latest .
